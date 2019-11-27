c4fefaf6efa829811cd00103233ba427d031c0ff
divyesh.parmar <divyesh.parmar@osf-commerce.com>
2019-11-27 22:34:50 +0530

    Add code with Service Request in flickr

--- cartridges/app_storefront_training/cartridge/controllers/Flickr.js
+++ cartridges/app_storefront_training/cartridge/controllers/Flickr.js
@@ -2,22 +2,22 @@
 
 var server = require('server');
 var HTTPClient = require('dw/net/HTTPClient');
- 
+var Service = require('dw/svc/Service');
+var HTTPService = require('dw/svc/HTTPService');
+
 server.get('Show',  function (req, res, next) {
+    
+    var flickrsGet = dw.svc.LocalServiceRegistry.createService("storefronttraining.http.flickr.get", {
+        createRequest: function(Service , args) {
+            Service.setRequestMethod("GET");
+        },
+        parseResponse: function(Service, client) {
+            return client.text;
+        },
+    });
+    var flickrStrObj = flickrsGet.call();
+    var flickrDataObj =  JSON.parse(flickrStrObj.object)
 
-    var httpClient = new HTTPClient();
-    httpClient.open('GET', 'https://api.flickr.com/services/feeds/photos_public.gne/?format=json&nojsoncallback=1');
-    httpClient.send();
-
-    if (httpClient.statusCode == 200) {
-        var flickrStrObj =  httpClient.text;
-        var flickrDataObj = JSON.parse(flickrStrObj);
-    }
-    else {
-        // error handling
-        flickrDataObj = "An error occurred with status code "+ httpClient.statusCode;
-    }
-     
     res.render('/flickr.isml', {
         flickrData : flickrDataObj, 
     });
